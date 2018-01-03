a340102efc1685bdc38f077ab5d40cf1689655b7
lixiantu <lxt1045@163.com>
2018-01-03 14:36:08 +0800

    added OpenCV

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -9,7 +9,8 @@
 
 #include "caffe2/core/init.h"
 
-//#include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
 
 #include <android/asset_manager.h>
 #include <android/asset_manager_jni.h>
@@ -41,6 +42,37 @@ void loadToNetDef(AAssetManager *mgr, caffe2::NetDef *net, const char *filename)
         alog("Couldn't parse net from data.\n");
     }
     AAsset_close(asset);
+}
+
+//for test
+extern "C"
+{
+JNIEXPORT jintArray JNICALL Java_net_johnhany_opencv_1ndk_MainActivity_grayProc(JNIEnv *env, jobject instance,
+                                                                                jintArray buf, jint w, jint h) {
+
+    jint *cbuf;
+    jboolean ptfalse = false;
+    cbuf = env->GetIntArrayElements(buf, &ptfalse);
+    if(cbuf == NULL){
+        return 0;
+    }
+
+    cv::Mat imgData(h, w, CV_8UC4, (unsigned char*)cbuf);
+
+    uchar* ptr = imgData.ptr(0);
+    for(int i = 0; i < w*h; i ++){
+        uchar grayScale = (uchar)(ptr[4*i+2]*0.299 + ptr[4*i+1]*0.587 + ptr[4*i+0]*0.114);
+        ptr[4*i+1] = grayScale;
+        ptr[4*i+2] = grayScale;
+        ptr[4*i+0] = grayScale;
+    }
+
+    int size=w * h;
+    jintArray result = env->NewIntArray(size);
+    env->SetIntArrayRegion(result, 0, size, cbuf);
+    env->ReleaseIntArrayElements(buf, cbuf, 0);
+    return result;
+}
 }
 
 extern "C" void
@@ -84,10 +116,10 @@ Java_facebook_f8demo_ClassifyCamera_classificationFromCaffe2(
 {
     {
         // 用opencv的方式读入文件
-//        cv::Mat bgr_img = cv::imread("imgs/cat.jpg", -1);
-//
-//        int height =  bgr_img.rows;
-//        int width = bgr_img.cols;
+        cv::Mat bgr_img = cv::imread("imgs/cat.jpg", -1);
+
+        int height =  bgr_img.rows;
+        int width = bgr_img.cols;
     }
     if (!_predictor)
     {
